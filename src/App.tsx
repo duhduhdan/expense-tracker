68b6363cdf0e4809664221bf71f8d206612aa0fd
Dan Loudon <loudon.dan@gmail.com>
2019-12-26 19:32:50 -0700

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Row, Col } from 'antd'
+
+import { ExpenseForm } from './ExpenseForm'
+
+import './App.css'
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Row>
+      <Col span={12} offset={6}>
+        <ExpenseForm />
+      </Col>
+    </Row>
+  )
 }
 
-export default App;
+export default App
